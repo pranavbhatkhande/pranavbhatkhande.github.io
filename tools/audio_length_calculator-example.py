b30184c7cc392c6268596821ef9d8c694d38d082
Pranav Bhatkhande <pranavbhatkhande@gmail.com>
2020-07-16 18:51:12 -0400

    Episode 4 is up

--- tools/audio_length_calculator-example.py
+++ tools/audio_length_calculator-example.py
@@ -4,13 +4,13 @@
 import os
 from mutagen.mp3 import MP3
 
-statinfo = os.stat('D:\\Podcasts\\Combustible Brains\\combustible-brains\\audio\\CBoA-Podcast-Ep3-AudioOnly.mp3')
+statinfo = os.stat('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3')
 size = str(statinfo.st_size)
 
-audio = MP3('D:\\Podcasts\\Combustible Brains\\combustible-brains\\audio\\CBoA-Podcast-Ep3-AudioOnly.mp3')
+audio = MP3('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3')
 length = str(audio.info.length)
  
-xmlfile = "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format('D:\\Podcasts\\Combustible Brains\\combustible-brains\\audio\\CBoA-Podcast-Ep3-AudioOnly.mp3',size)
+xmlfile = "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3',size)
 xmlfile += "<itunes:duration>{}</itunes:duration>\n".format(length)
 
 print(xmlfile)
