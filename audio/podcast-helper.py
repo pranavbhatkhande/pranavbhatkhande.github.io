2e862222f7734c519d95110c45b58fd079787800
Pranav Bhatkhande <pranavbhatkhande@gmail.com>
2021-10-10 14:25:30 -0400

    audio folder is now a docker container.

--- audio/podcast-helper.py
+++ audio/podcast-helper.py
@@ -5,11 +5,12 @@ import os
 import sys
 from mutagen.mp3 import MP3
 
-mp3filename=str(sys.argv[0])
+mp3filename=str(sys.argv[1])
+print(mp3filename)
 statinfo = os.stat(mp3filename)
 size = str(statinfo.st_size)
 
-audio = MP3('CBoA-Podcast-Ep4-AudioOnly.mp3')
+audio = MP3(mp3filename)
 length = str(audio.info.length)
  
 xmlfile = "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3',size)
