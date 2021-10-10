d1c3605e5964a8469f011d9f47a3417375049acc
Pranav Bhatkhande <pranavbhatkhande@gmail.com>
2021-10-10 13:48:09 -0400

    Dockerize scripts

--- tools/audio_length_calculator-example.py
+++ tools/audio_length_calculator-example.py
@@ -2,12 +2,14 @@
 # TODO(pranav) test this or something.
 
 import os
+import sys
 from mutagen.mp3 import MP3
 
-statinfo = os.stat('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3')
+mp3filename=str(sys.argv[0])
+statinfo = os.stat(mp3filename)
 size = str(statinfo.st_size)
 
-audio = MP3('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3')
+audio = MP3('CBoA-Podcast-Ep4-AudioOnly.mp3')
 length = str(audio.info.length)
  
 xmlfile = "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format('/Users/pranav/sandboxes/pranavbhatkhande.github.io/audio/CBoA-Podcast-Ep4-AudioOnly.mp3',size)
--- tools/audio_length_calculator.py
+++ tools/audio_length_calculator.py
@@ -4,11 +4,11 @@
 import os
 from mutagen.mp3 import MP3
 
-        statinfo = os.stat(mp3filename)
-        size = str(statinfo.st_size)
+statinfo = os.stat(mp3filename)
+size = str(statinfo.st_size)
 
-        audio = MP3(mp3filename)
-        length = str(audio.info.length)
+audio = MP3(mp3filename)
+length = str(audio.info.length)
 
-        xmlfile += "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format(mp3filename,size)
-        xmlfile += "<itunes:duration>{}</itunes:duration>\n".format(length)
+xmlfile += "<enclosure url='https://PODCAST URL/{}' length='{}' type='audio/mpeg'/>\n".format(mp3filename,size)
+xmlfile += "<itunes:duration>{}</itunes:duration>\n".format(length)
